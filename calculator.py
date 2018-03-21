d73af6ad95c51c0e449ea5009de1eb5a383ea164
Lucia <lblackwe@udel.edu>
2018-03-21 13:59:15 -0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,19 +1,57 @@
+
+
 class Calculator(object):
     def __init__(self, last_result=0, input=None):
+        """Command line calculator. Takes input until 'q' or '=' is entered,
+        then either quits ('q') or evaluates the previous input ('=').
+
+        Args:
+            last_result (float): Result of the last completed calculation. Used
+                for testing. Defaults to 0.
+            input (List[char]): User input in the form of a list of single
+                characters. Example: ['1', '+', '2']. Used for testing. If no
+                list is given, an empty list is used. Defaults to None.
+        """
         self.last_result = last_result
         if input is None:
             self.input = []
         else:
             self.input = input
 
-    def perform_calculation(self):
-        if len(self.input) <= 0:
-            return
+    def _is_number(self, n):
+        """Determines whether a string represents a number, including negatives
+        (unlike isdigit).
 
-        # Remove whitespace
-        # TODO
+        Args:
+            n (str): String to evaluate
 
-        # Find multiple-digit numbers
+        Returns:
+            bool: True if the string contains only a positive or negative
+                number, otherwise False
+        """
+        if n.lstrip('-').isdigit():
+            return True
+        return False
+
+    def _condense_negatives(self):
+        """Loops through self.input and condenses any negative signs with their
+        corresponding digits. Assumes input starting with a - is a negative,
+        rather than a subtraction from the previous result.
+        """
+        for i in range(0, len(self.input)):
+            try:
+                if self.input[i] == '-':
+                    if i == 0 or self._is_number(self.input[i - 1]) is False:
+                        self.input[i] += self.input[i + 1]
+                        del(self.input[i + 1])
+                        i -= 1
+            except IndexError:
+                continue
+
+    def _condense_multi_digit_numbers(self):
+        """Loops through self.input and condenses any single digits with any
+        other adjacent digits.
+        """
         for i in range(0, len(self.input) - 1):
             try:
                 if self.input[i].isdigit():
@@ -25,8 +63,11 @@ class Calculator(object):
             except IndexError:
                 continue
 
-        # Find decimals
-        for i in range(0, len(self.input) - 1):
+    def _condense_decimals(self):
+        """Loops through self.input and condenses any decimal points with their
+        surrounding digits.
+        """
+        for i in range(0, len(self.input)):
             try:
                 if self.input[i] == '.':
                     # Attach it to the surrounding digits
@@ -36,30 +77,128 @@ class Calculator(object):
                     if i > 0 and self.input[i - 1].isdigit():
                         self.input[i] = self.input[i - 1] + self.input[i]
                         to_decrement += 1
+                        del_before = True
                     if i < len(self.input) + 1 and self.input[i + 1].isdigit():
                         self.input[i] += self.input[i + 1]
                         to_decrement += 1
-                    i -= to_decrement
+                        del_after = True
                     if del_before is True:
                         del(self.input[i - 1])
                     if del_after is True:
-                        del(self.input[i + 1])
+                        del(self.input[i])
+                    i -= to_decrement
             except IndexError:
                 continue
 
+    def _operate(self, operation, index):
+        """Performs the indicated operation on self.input and returns the number
+        of indices the calculation discovery loop should decrement (due to
+        deleted list items).
+
+        Args:
+            operation (char): *, /, +, or - -- the operation to perform
+            index (int): The index of self.input currently being evaluated
+
+        Returns:
+            int: The number of indices the calling loop should decrement due
+                to list items being deleted
+        """
+        # If this is the first char, multiply current result
+        if index == 0:
+            first = self.last_result
+        # Otherwise, multiply surrounding input
+        else:
+            first = float(self.input[index - 1])
+
+        if operation == '*':
+            self.input[index] = str(first * float(self.input[index + 1]))
+        elif operation == '/':
+            self.input[index] = str(first / float(self.input[index + 1]))
+        elif operation == '+':
+            self.input[index] = str(first + float(self.input[index + 1]))
+        elif operation == '-':
+            self.input[index] = str(first - float(self.input[index + 1]))
+
+        if index == 0:
+            del(self.input[index + 1])
+            return 1
+        else:
+            del(self.input[index + 1])
+            del(self.input[index - 1])
+            return 2
+
+    def _multiply_and_divide(self):
+        """Loops through self.input and performs any multiplication and
+        division operations in left-to-right order.
+        """
+        # Look for * or /
+        while '*' in self.input or '/' in self.input:
+            for i in range(0, len(self.input)):
+                try:
+                    if self.input[i] in ['*', '/']:
+                        i -= self._operate(self.input[i], i)
+                except IndexError:
+                    continue
+
+    def _add_and_subtract(self):
+        """Loops through self.input and performs any addition and subtraction
+        operations in left-to-right order.
+        """
+        while '+' in self.input or '-' in self.input:
+            for i in range(0, len(self.input)):
+                try:
+                    if self.input[i] in ['+', '-']:
+                        i -= self._operate(self.input[i], i)
+                except IndexError:
+                    continue
+
+    def perform_calculation(self):
+        """Evaluates any input entered registered '=' was entered. Clears
+        input for the next calculation."""
+        if len(self.input) <= 0:
+            return
+
+        # Remove spaces
+        self.input = [x for x in self.input if x != ' ']
+
+        # Find negatives
+        self._condense_negatives()
+
+        # Find multiple-digit numbers
+        self._condense_multi_digit_numbers()
+
+        # Find decimals
+        self._condense_decimals()
+
         # If first piece of input is a digit, start a new calculation
-        if self.input[0] is not None and self.input[0].isdigit():
-            self.last_result = float(self.input[0])
+        if self.input[0] is not None:
+            try:
+                float(self.input[0])
+                self.last_result = 0
+            except ValueError:
+                pass
+
+        # Perform operations in order -- (PE)MDAS and left-to-right
+        self._multiply_and_divide()
+        self._add_and_subtract()
+
+        # Save result
+        self.last_result = float(self.input[0])
 
         # Clear input
         self.input = []
 
-
     def print_output(self):
-        print self.last_result
+        """Prints the latest calculation result to stdout."""
+        print(self.last_result)
 
     def get_input(self):
-        char = raw_input()
+        """Retrieves input from command line. Accepts input until user enters
+        'q', which will quit the program, or '=', which will evaluate entered
+        input."""
+        print('Enter equation:')
+        char = getch()
+        print(char)
         if char.lower() == 'q':
             return False
         elif char == '=':
@@ -73,7 +212,7 @@ class Calculator(object):
 
 def main():
     calc = Calculator()
-    run = False
+    run = True
     while run is True:
         run = calc.get_input()
 
--- tests.py
+++ tests.py
@@ -4,157 +4,189 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
     # Input
-    def test_no_ops(self):
+    def test_no_ops_single(self):
         # Single digit
         c = calculator.Calculator(input=['5'])
         c.perform_calculation()
         self.assertEqual(c.last_result, 5)
 
+    def test_no_ops_double(self):
         # Double-digit integer
         c = calculator.Calculator(input=['4', '5'])
         c.perform_calculation()
         self.assertEqual(c.last_result, 45)
 
+    def test_no_ops_multiple(self):
         # Multiple-digit decimal
         c = calculator.Calculator(input=['4', '5', '.', '6', '7'])
         c.perform_calculation()
         self.assertEqual(c.last_result, 45.67)
 
+    def test_no_ops_negative(self):
+        # Negative single digit
+        c = calculator.Calculator(input=['-', '3'])
+        c.perform_calculation()
+        self.assertEqual(c.last_result, -3)
+
     # Addition
-    def test_addition(self):
+    def test_addition_positive(self):
         # All positive
         c = calculator.Calculator(input=['1', '+', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 3)
+        self.assertEqual(c.last_result, 1 + 2)
 
+    def test_addition_neg_to_pos(self):
         # Add negative to positive
         c = calculator.Calculator(input=['3', '+', '-', '1'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 2)
+        self.assertEqual(c.last_result, 3 + -1)
 
+    def test_addition_pos_to_neg(self):
         # Add positive to negative
         c = calculator.Calculator(input=['-', '1', '+', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 2)
+        self.assertEqual(c.last_result, -1 + 3)
 
+    def test_addition_neg_result(self):
         # Get a negative result
         c = calculator.Calculator(input=['1', '+', '-', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, -2)
+        self.assertEqual(c.last_result, 1 + -3)
 
+    def test_addition_decimals(self):
         # With decimals
         c = calculator.Calculator(input=['1', '.', '4', '+', '2', '.', '5'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 3.9)
+        self.assertEqual(c.last_result, 1.4 + 2.5)
 
     # Subtraction
-    def test_subtraction(self):
+    def test_subtraction_pos(self):
         # All positive
         c = calculator.Calculator(input=['3', '-', '1'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 2)
+        self.assertEqual(c.last_result, 3 - 1)
 
+    def test_subtraction_neg_from_pos(self):
         # Subtract negative from positive
         c = calculator.Calculator(input=['3', '-', '-', '1'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 4)
+        self.assertEqual(c.last_result, 3 - -1)
 
+    def test_subtraction_pos_from_neg(self):
         # Subtract positive from negative (bonus: negative result)
         c = calculator.Calculator(input=['-', '1', '-', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, -4)
+        self.assertEqual(c.last_result, -1 - 3)
 
+    def test_subtraction_decimals(self):
         # With decimals
         c = calculator.Calculator(input=['4', '.', '2', '-', '1', '.', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 2.9)
+        self.assertEqual(c.last_result, 4.2 - 1.3)
 
     # Multiplication
-    def test_multiplication(self):
+    def test_multiplication_pos(self):
         # All positive
         c = calculator.Calculator(input=['3', '*', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 6)
+        self.assertEqual(c.last_result, 3 * 2)
 
+    def test_mult_pos_by_neg(self):
         # Multiply positive by negative
         c = calculator.Calculator(input=['3', '*', '-', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, -6)
+        self.assertEqual(c.last_result, 3 * -2)
 
+    def test_mult_both_neg(self):
         # Multiply two negatives
         c = calculator.Calculator(input=['-', '3', '*', '-', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 6)
+        self.assertEqual(c.last_result, -3 * -2)
 
+    def test_mult_decimals(self):
         # With decimals
         c = calculator.Calculator(input=['3', '.', '1', '*', '2', '.', '5'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 7.75)
+        self.assertEqual(c.last_result, 3.1 * 2.5)
 
     # Division
     def test_division(self):
         # All positive
         c = calculator.Calculator(input=['6', '/', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 3)
+        self.assertEqual(c.last_result, 6 / 2)
 
+    def test_div_pos_by_neg(self):
         # Divide positive by negative
         c = calculator.Calculator(input=['6', '/', '-', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, -3)
+        self.assertEqual(c.last_result, 6 / -2)
 
+    def test_div_two_negs(self):
         # Divide two negatives
         c = calculator.Calculator(input=['-', '6', '/', '-', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 3)
+        self.assertEqual(c.last_result, -6 / -2)
 
-        # With decimals
+    def test_div_decimal_result(self):
+        # With decimal result
         c = calculator.Calculator(input=['7', '/', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 3.5)
+        self.assertEqual(c.last_result, 7 / 2)
+
+    def test_div_decimals(self):
+        # With decimal result
+        c = calculator.Calculator(input=['2', '.', '4', '2', '/', '1', '.', '1'])
+        c.perform_calculation()
+        self.assertEqual(c.last_result, 2.42 / 1.1)
 
     # Multiple operations
     def test_multiple_same_operations(self):
         # Addition
         c = calculator.Calculator(input=['1', '+', '2', '+', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 6)
+        self.assertEqual(c.last_result, 1 + 2 + 3)
 
+    def test_mult_ops_sub(self):
         # Subtraction
         c = calculator.Calculator(input=['8', '-', '2', '-', '1'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 5)
+        self.assertEqual(c.last_result, 8 - 2 - 1)
 
+    def test_mult_ops_mult(self):
         # Multiplication
         c = calculator.Calculator(input=['4', '*', '2', '*', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 24)
+        self.assertEqual(c.last_result, 4 * 2 * 3)
 
+    def test_mult_ops_div(self):
         # Division
         c = calculator.Calculator(input=['24', '/', '3', '/', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 4)
+        self.assertEqual(c.last_result, 24 / 3 / 2)
 
     # Order of operations
     def test_order_of_operations(self):
         # All positive
         c = calculator.Calculator(input=['1', '+', '2', '*', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 7)
+        self.assertEqual(c.last_result, 1 + 2 * 3)
 
+    def test_ooo_neg(self):
         # Including negatives
         c = calculator.Calculator(input=['-', '1', '+', '2', '*', '3'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 5)
+        self.assertEqual(c.last_result, -1 + 2 * 3)
 
     # Continuing from previous line
     def test_continuing_operations(self):
         # All positive
         c = calculator.Calculator(last_result=4, input=['+', '2'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, 6)
+        self.assertEqual(c.last_result, 4 + 2)
 
+    def test_continue_neg(self):
         # Including negatives
         c = calculator.Calculator(last_result=2, input=['-', '6'])
         c.perform_calculation()
-        self.assertEqual(c.last_result, -4)
+        self.assertEqual(c.last_result, -6)
